2ab122ea1eca3afa2988b69c497546793473363f
saidi <saidi.ahamada@gmail.com>
2021-12-03 17:35:18 +0100

    improve ui/ux

--- components/TodoList.js
+++ components/TodoList.js
@@ -9,8 +9,8 @@ app.component('todo-list', {
         <div class="todo-list">
             <div v-for="item in items" class="todo-item" :key="item.id">
                 <div class="todo-item-display">
-                    <input type="checkbox" :checked="item.checked" @click="terminateItem(item.id)"/>
-                    <label>{{item.title}}</label>
+                    <input type="checkbox" :checked="item.checked" @click="terminateItem(item.id)" :id="item.id"/>
+                    <label :for="item.id">{{item.title}}</label>
                 </div>
                 <div class="todo-item-action">
                     <button @click="removeItem(item.id)">Remove</button>
